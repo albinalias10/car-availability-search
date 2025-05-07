a053f6463f04cdc2fc506ef36401d06654258f8a
albinalias10 <albinalias10@gmail.com>
2025-05-07 20:14:03 +0100

    CarAvailProj: Implementing Header Component to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <h1>My React App</h1>
-      </div>
+      <Header />
     </>
   )
 }
